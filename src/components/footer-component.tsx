ff9f80991651d8a1c7f8d669bfd26f4462457b9a
zmarc3ll <zubermarcellpetrik@gmail.com>
2023-03-28 09:38:57 +0200

    footer done

---
+++ src/components/footer-component.tsx
@@ -0,0 +1,11 @@
+import { Component } from "react";
+
+export default class Footer extends Component {
+    render() {
+        return <>
+            <footer>
+                <p>Készítette: Zuber Marcell</p>
+            </footer>
+        </>
+    }
+}
