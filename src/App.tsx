b5920ce95516b1bd659edd75d2f55c2bf4f45f0a
zmarc3ll <zubermarcellpetrik@gmail.com>
2023-03-28 09:38:36 +0200

    added routes, navbar and footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap';
 import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import Navbar from './components/navbar-component';
+import Footer from './components/footer-component';
+import Fooldal from './components/pages/Fooldal';
+import SzakmaiTapasztalat from './components/pages/SzakmaiTapasztalat';
+import IdopontFoglalas from './components/pages/IdopontFoglalas';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <><Navbar></Navbar>
+     <Routes>
+      <Route path='/' element={<Fooldal />} />
+      <Route path='/tapasztalat' element={<SzakmaiTapasztalat />} />
+      <Route path='/idopont' element={<IdopontFoglalas />} />
+    </Routes>
+    <Footer></Footer>
+    </>
   );
 }
 
